badac524b975e65906516a167891e929ec8262d2
Leolionross <62186470+Leolionross@users.noreply.github.com>
2023-07-05 23:25:20 -0400

    easier to see varies (made this edit from my phone so i can't test this)

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import requests
 from time import sleep
 
+channelid = '[CHANNELID]'
+token = '[DISCORDTOKEN]'
+
 print('script is running')
 
 while True:
-    typing = requests.post('https://discord.com/api/v9/channels/[CHANNELID]/typing', headers={'authorization': '[DISCORDTOKEN]'})
+    typing = requests.post(f'https://discord.com/api/v9/channels/{channelid}/typing', headers={'authorization': token})
     if not typing.status_code == 204:
         print('error')
         print(f'status code: {typing.status_code}')
